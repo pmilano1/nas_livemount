0308bbe1d6166cd74860774541f1b22f8a61b010
Peter J. Milanese <peter.milanese@rubrik.com>
2019-05-26 21:24:59 -0400

    Initial

--- fuse_sample.py
+++ fuse_sample.py
@@ -26,9 +26,9 @@ class RubrikFS(LoggingMixIn, Operations):
             path = re.sub(r'^\/(\S+.*)', '\\1', path)
             if not path.startswith("/"):
                 path = path.replace('/', '\\')
-                (path, name) = path.rsplit('\\', 1)
-        print(path)
-        print(name)
+                (path) = path.rsplit('\\', 1)
+        print(path[0])
+        print(path[1])
         st = os.lstat('/tmp')
         return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
                                                         'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size',
