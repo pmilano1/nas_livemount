1ee31eb0df3ce21f9e618fb184ded0222bdaab96
Peter J. Milanese <peter.milanese@rubrik.com>
2019-06-04 13:19:10 -0400

    Updated snap to test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "Updating Packages"
-sudo apt update -y
+#echo "Updating Packages"
+#sudo apt update -y
 echo "Install pip3"
 sudo apt install -y python3-pip
 echo "Install fuse"
-sudo pip3 install fuse requests
+sudo pip3 install fusepy requests
 echo "Clone repository"
 git clone https://github.com/pmilano1/nas_livemount.git
+chown -R vagrant .
+echo "user_allow_opther" >> /etc/fuse.conf
