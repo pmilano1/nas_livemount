abd3de0b5f6d07b1e86bd62979c44582a83bfa34
Peter J. Milanese <peter.milanese@rubrik.com>
2019-06-12 15:59:52 -0400

    Updated snap to test

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #echo "Updating Packages"
-#sudo apt update -y
-echo "Install pip3"
-sudo apt install -y python3-pip
+sudo apt update -y
+echo "Install pip3 and db libs"
+sudo apt install -y python3-pip libpq-dev
 echo "Install fuse"
-sudo pip3 install fusepy requests
+sudo pip3 install fusepy requests psycopg2
 echo "Clone repository"
 git clone https://github.com/pmilano1/nas_livemount.git
 echo "user_allow_other" >> /etc/fuse.conf
 mkdir nas_livemount/tmp
 chown -R vagrant:vagrant .
+cd nas_livemount
+git checkout cachedb
