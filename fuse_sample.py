1deac7519aae419dc1f81f2d89962c0eadf9cb0d
Peter J. Milanese <peter.milanese@rubrik.com>
2019-06-12 10:23:15 -0400

    Updated snap to test

--- fuse_sample.py
+++ fuse_sample.py
@@ -131,7 +131,7 @@ class RubrikFS(LoggingMixIn, Operations):
         # Modify path if its a windows volume
         if re.search(r'^/[A-Z]:', path):
             path = re.sub(r'^\/', "", path)
-            path = re.sub(r'/', "\\", path)
+            path = re.sub(r'\/', "\\", path)
 
         # Seed directory array for navigation
         objs = ['.', '..']
